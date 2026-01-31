e5dfc8f6c18790cefda1f158a7a689b5232035a9
Sivaprasad2109 <sivamaha2109@gmail.com>
2026-01-31 11:56:48 +0530

    Update server log URL to production endpoint

--- server.js
+++ server.js
@@ -114,5 +114,5 @@ app.post("/analyze", upload.single("resume"), async (req, res) => {
 });
 
 app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+  console.log("https://ats-backend-hoqv.onrender.com");
 });
